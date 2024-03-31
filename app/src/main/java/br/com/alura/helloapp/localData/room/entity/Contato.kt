9324b2f6836b8a72639760b12ddc12ace4a0d5ae
rafaseron <rafaelseron3011@gmail.com>
2024-03-31 15:38:53 -0400

    add ColumInfo to Entity

--- app/src/main/java/br/com/alura/helloapp/localData/room/entity/Contato.kt
+++ app/src/main/java/br/com/alura/helloapp/localData/room/entity/Contato.kt
@@ -1,5 +1,6 @@
 package br.com.alura.helloapp.localData.room.entity
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.util.*
@@ -7,10 +8,16 @@ import java.util.*
 @Entity
 data class Contato(
     @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
     val id: Long = 0L,
+    @ColumnInfo(name = "nome")
     val nome: String = "",
+    @ColumnInfo(name = "sobrenome")
     val sobrenome: String = "",
+    @ColumnInfo(name = "telefone")
     val telefone: String = "",
+    @ColumnInfo(name = "fotoPerfil")
     val fotoPerfil: String = "",
+    @ColumnInfo(name = "aniversario")
     val aniversario: Date? = null,
 )
