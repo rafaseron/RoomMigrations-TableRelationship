bc222743e7340cb0fe6e3c8e5e76dcd07f97835a
rafaseron <rafaelseron3011@gmail.com>
2024-04-02 03:01:53 -0400

    add Uuario Entity

--- app/src/main/java/br/com/alura/helloapp/localData/room/database/HelloAppDatabase.kt
+++ app/src/main/java/br/com/alura/helloapp/localData/room/database/HelloAppDatabase.kt
@@ -6,8 +6,9 @@ import androidx.room.TypeConverters
 import br.com.alura.helloapp.localData.room.converter.TypeConverter
 import br.com.alura.helloapp.localData.room.entity.Contato
 import br.com.alura.helloapp.localData.room.dao.ContatoDao
+import br.com.alura.helloapp.localData.room.entity.Usuario
 
-@Database(entities = [Contato::class], version = 1)
+@Database(entities = [Contato::class, Usuario::class], version = 1)
 @TypeConverters(TypeConverter::class)
 abstract class HelloAppDatabase : RoomDatabase() {
     abstract fun contatoDao(): ContatoDao
---
+++ app/src/main/java/br/com/alura/helloapp/localData/room/entity/Usuario.kt
@@ -0,0 +1,16 @@
+package br.com.alura.helloapp.localData.room.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Usuario (
+    @ColumnInfo(name = "name")
+    val name: String,
+    @PrimaryKey
+    @ColumnInfo(name = "username")
+    val username: String,
+    @ColumnInfo(name = "password")
+    val password: String
+)
