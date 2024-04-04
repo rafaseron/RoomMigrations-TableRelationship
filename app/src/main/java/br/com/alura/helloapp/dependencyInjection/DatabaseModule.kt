bb9d3d21b11818291ff902d4b60ca7e655600db6
rafaseron <rafaelseron3011@gmail.com>
2024-04-03 21:34:44 -0400

    MIGRATION_1_2 Manualmente no Room DB

--- app/src/main/java/br/com/alura/helloapp/dependencyInjection/DatabaseModule.kt
+++ app/src/main/java/br/com/alura/helloapp/dependencyInjection/DatabaseModule.kt
@@ -2,6 +2,8 @@ package br.com.alura.helloapp.di.module
 
 import android.content.Context
 import androidx.room.Room
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import br.com.alura.helloapp.localData.room.dao.ContatoDao
 import br.com.alura.helloapp.localData.room.database.HelloAppDatabase
 import br.com.alura.helloapp.localData.room.repository.ContatoRepository
@@ -26,7 +28,18 @@ class DatabaseModule {
             context,
             HelloAppDatabase::class.java,
             DATABASE_NAME
-        )/*.fallbackToDestructiveMigration()*/.build()
+        )/*.fallbackToDestructiveMigration()*/
+            /*.addMigrations(Migrations.MIGRATION_1_2)*/
+            .build()
+    }
+
+    object Migrations{
+        val MIGRATION_1_2 = object: Migration(1,2){
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("CREATE TABLE IF NOT EXISTS 'Usuario' ('name' TEXT NOT NULL, 'username' TEXT NOT NULL, 'password' TEXT NOT NULL, PRIMARY KEY('username'))")
+            }
+
+        }
     }
 
     @Provides
